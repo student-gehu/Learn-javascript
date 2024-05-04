bd834a346849ff7e8b32635ecdfb120fda4ccb6a
Rahul Rana <163443364+student-gehu@users.noreply.github.com>
2024-05-05 01:05:50 +0530

    Update practice.js

--- ch6/practice.js
+++ ch6/practice.js
@@ -1,1 +1,50 @@
+//CH->6   Question->1
 
+/*let age=prompt("Enter you age")
+age=Number.parseInt(age)
+
+const canDrive=(age)=>{
+    return age>=18?true:false
+}
+if(canDrive(age)){
+    alert("yes you can drive")
+}else{
+    alert("you cannot drive")
+}*/
+
+//CH->6   Question->2
+
+/*
+let age=prompt("Enter you age")
+age=Number.parseInt(age)
+const canDrive=(age)=>{
+    return age>=18?true:false
+}
+let runagain=true
+while(runagain){
+    let age=prompt("Enter you age")
+    age=Number.parseInt(age)
+    if(age<0){
+        console.error("Fuck you enter real age nonsence!");
+        break;
+    }
+    if(canDrive(age)){
+    alert("yes you can drive")
+    }else{
+    alert("you cannot drive")
+    }
+    runagain=confirm("do you want to run again")
+}
+*/
+
+//CH->6   Question->3  redirect to google.com if number is less than 4
+
+let number=prompt("Enter the number");
+number=Number.parseInt(number)
+
+if(number<4){
+    location.href="https://google.com"
+}
+
+let color=prompt("Enter the page backgroung color")
+document.body.style.background=color
